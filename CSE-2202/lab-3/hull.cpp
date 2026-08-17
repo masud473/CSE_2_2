44c263afcd8ed46621637b877027f12fb7c9f32b
Md. Farhan Masud <mdfarhanmasud584@gmail.com>
2026-08-18 00:56:10 +0600

    graham scan

--- CSE-2202/lab-3/hull.cpp
+++ CSE-2202/lab-3/hull.cpp
@@ -1,65 +1,117 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 #include <cstdlib>
 #include <vector>
 using namespace std;
-vector<vector<int>> ans;
-int side(vector<int> a, vector<int> b, vector<int> p) {
-  return (b[0] - a[0]) * (p[1] - a[1]) - (b[1] - a[1]) * (p[0] - a[0]);
+class Point {
+public:
+  double x, y;
+  Point(double a, double b) : x(a), y(b) {};
+  Point() {}
+  Point(vector<double> v) {
+    this->x = v[0];
+    this->y = v[1];
+  }
+  void operator=(const Point &other) {
+    this->x = other.x;
+    this->y = other.y;
+  }
+  bool operator==(const Point &other) {
+    return this->x == other.x && this->y == other.y;
+  }
+  bool operator!=(const Point &other) {
+    return this->x != other.x && this->y != other.y;
+  }
+  void print() { cout << this->x << "," << this->y << " "; }
+};
+vector<Point> ans;
+double cmp(Point &a, Point &b, Point &c) {
+  return (b.y - c.y) * (b.x - a.x) - (b.x - c.x) * (b.y - a.y);
 }
-int height(vector<int> a, vector<int> b, vector<int> p) {
-  return abs(side(a, b, p));
-}
-int hull_point(vector<vector<int>> &arr, vector<int> &a, vector<int> &b) {
+int pivot(vector<Point> &arr, Point &left, Point &right) {
   int i = 0;
   for (int j = 1; j < arr.size(); j++) {
-    if (height(a, b, arr[j]) > height(a, b, arr[i])) {
+    if (abs(cmp(left, right, arr[j])) > abs(cmp(left, right, arr[i]))) {
       i = j;
     }
   }
   return i;
 }
-
-void hull(vector<vector<int>> &arr, vector<int> &a, vector<int> &b) {
+void hull(vector<Point> &arr, Point &left, Point &right) {
   if (arr.empty()) {
     return;
   }
-  int pivot = hull_point(arr, a, b);
-  vector<int> pivot_point = arr[pivot];
-  ans.push_back(pivot_point);
-  vector<vector<int>> top_hull, bottom_hull;
+  Point p = arr[pivot(arr, left, right)];
+  vector<Point> top, bottom;
+  ans.push_back(p);
   for (auto &i : arr) {
-    if (side(a, pivot_point, i) > 0) {
-      top_hull.push_back(i);
-    } else if (side(pivot_point, b, i) > 0) {
-      bottom_hull.push_back(i);
+    if (cmp(left, p, i) > 0) {
+      top.push_back(i);
+    } else if (cmp(p, right, i) > 0) {
+      bottom.push_back(i);
     }
   }
-  hull(top_hull, a, pivot_point);
-  hull(bottom_hull, pivot_point, b);
+  hull(top, left, p);
+  hull(bottom, p, right);
 }
 int main() {
-  int x = height({-10, 0}, {10, 0}, {0, -9});
-  vector<vector<int>> arr = {{10, 0}, {-10, 0}, {0, 10}, {0, -10},
-                             {0, 3},  {0, -3},  {3, 0},  {-3, 0}};
-  sort(arr.begin(), arr.end(),
-       [](const vector<int> &a, const vector<int> &b) { return a[0] < b[0]; });
-  vector<vector<int>> top_hull, bottom_hull;
-  vector<int> a = arr[0], b = arr.back();
+  vector<Point> arr = {{10, 0}, {-10, 0}, {0, 10}, {0, -10},
+                       {0, 3},  {0, -3},  {3, 0},  {-3, 0}};
+  Point left = arr[0], right = arr[0];
   for (auto &i : arr) {
-    if (side(a, b, i) > 0) {
-      top_hull.push_back(i);
-    } else if (side(b, a, i) > 0) {
-      bottom_hull.push_back(i);
+    if (left.x > i.x) {
+      left = i;
+    } else if (left.x == i.x) {
+      if (i.y > left.y) {
+        left = i;
+      }
+    }
+    if (right.x < i.x) {
+      right = i;
+    } else if (right.x == i.x) {
+      if (i.y > right.y) {
+        right = i;
+      }
     }
   }
-
-  ans.push_back(a);
-  ans.push_back(b);
-  hull(top_hull, a, b);
-  hull(bottom_hull, b, a);
-  for (auto &i : ans) {
-    cout << i[0] << " " << i[1] << '\n';
+  ans.push_back(left);
+  ans.push_back(right);
+  vector<Point> top, bottom;
+  for (auto &i : arr) {
+    if (cmp(left, right, i) > 0) {
+      top.push_back(i);
+    }
   }
-  return 0;
+  hull(top, left, right);
+  left = arr[0], right = arr[0];
+  for (auto &i : arr) {
+    if (left.x > i.x) {
+      left = i;
+    } else if (left.x == i.x) {
+      if (i.y < left.y) {
+        left = i;
+      }
+    }
+    if (right.x < i.x) {
+      right = i;
+    } else if (right.x == i.x) {
+      if (i.y < right.y) {
+        right = i;
+      }
+    }
+  }
+  if (left != ans[0]) {
+    ans.push_back(left);
+  }
+  if (right != ans[1]) {
+    ans.push_back(right);
+  }
+  for (auto &i : arr) {
+    if (cmp(right, left, i) > 0) {
+      bottom.push_back(i);
+    }
+  }
+  hull(bottom, right, left);
+  for (auto &i : ans) {
+    i.print();
+  }
 }
---
+++ CSE-2202/lab-3/hull2.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+class Point {
+public:
+  double x, y;
+  Point(double a, double b) : x(a), y(b) {};
+  Point() {}
+  Point(vector<double> v) {
+    this->x = v[0];
+    this->y = v[1];
+  }
+  void operator=(const Point &other) {
+    this->x = other.x;
+    this->y = other.y;
+  }
+  bool operator==(const Point &other) {
+    return this->x == other.x && this->y == other.y;
+  }
+  bool operator!=(const Point &other) {
+    return this->x != other.x && this->y != other.y;
+  }
+  void print() { cout << this->x << "," << this->y << " "; }
+};
+vector<Point> ans;
+double cmp(const Point &a, const Point &b, const Point &c) {
+  return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+}
+double dist(const Point &a, const Point &b) {
+  return (a.x - b.x) * (a.x - b.x) - (a.y - b.y) * (a.y - b.y);
+}
+int main() {
+  vector<Point> arr = {{10, 0}, {-10, 0}, {0, 10}, {0, -10},
+                       {0, 3},  {0, -3},  {3, 0},  {-3, 0}};
+  Point p =
+      *min_element(arr.begin(), arr.end(), [](const Point &a, const Point &b) {
+        if (a.y == b.y) {
+          return a.x < b.x;
+        }
+        return a.y < b.y;
+      });
+  sort(arr.begin(), arr.end(), [&p](const Point &a, const Point &b) {
+    double ccw = cmp(a, b, p);
+    if (ccw == 0) {
+      return dist(a, p) < dist(b, p);
+    }
+    return ccw > 0;
+  });
+
+  for (int i = 0; i < arr.size(); i++) {
+    while (ans.size() > 1 &&
+           cmp(*(ans.end() - 2), *(ans.end() - 1), arr[i]) <= 0) {
+      ans.pop_back();
+    }
+    ans.push_back(arr[i]);
+  }
+  for (auto &i : ans) {
+    i.print();
+  }
+}
